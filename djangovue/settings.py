c75ef4be9bfbf02818accd805d8a8e99163e14a4
yess-guns <yessy12384@gmail.com>
2020-06-19 00:53:32 -0500

    crear estructura de la REST API

--- djangovue/settings.py
+++ djangovue/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listelement'
+    'listelement',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
